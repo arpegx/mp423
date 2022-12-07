f3827b295e793ea54dc048028a8ad5a608932b76
larbaer <110061080+larbaer@users.noreply.github.com>
2022-12-07 20:13:28 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+#  Die Idee:
+    Ein kurzes Script um mp4-Dateien in mp3-Dateien umzuwandeln. Die Dateien werden
+    in einen Zwischenordner verschoben. Wahlweise liegen dort  sowie so die
+    eigenen Musik - Ordner.
+    Die mp4-Dateien werden im aktuellen Verzeichnis gesucht.
+    
+# Dependencies:
+    ffmpeg
+    
+# Anwendung:
+    Just type
+    
+    $ ./mp423
+    
+    mit der Binary im selben Verzeichnis wie die zu konvertierenden mp4's
---
+++ mp423.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+#                  _  _  ____  _____ 
+#  _ __ ___  _ __ | || ||___ \|___ / 
+# | '_ ` _ \| '_ \| || |_ __) | |_ \ 
+# | | | | | | |_) |__   _/ __/ ___) |
+# |_| |_| |_| .__/   |_||_____|____/ 
+#           |_|                      
+# https://github.com/larbaer 
+
+# FUNCTION-----------------------------------------------------------
+mp423(){
+# creates Folder to store mp3-files
+	if [ ! -d ./mp3-files ]; then
+		mkdir ./mp3-files;
+	fi;
+
+# converts all MP4-files to MP3 inside the directory and stores them
+	for file in ./*.mp4 ; do
+		ffmpeg -i "$file" "${file%.mp4}.mp3";
+		mv "${file%.mp4}.mp3" ./mp3-files;
+	done;
+}
+
+# MAIN---------------------------------------------------------------
+
+# Get the thing done
+mp423
